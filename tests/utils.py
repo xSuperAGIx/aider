ebd01ff8235cdcb529c8e9e244c570462fe96d35
OpenAI <136945467+xSuperAGIx@users.noreply.github.com>
2024-03-16 01:51:36 +0200

    improve

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,8 @@
 import os
 import tempfile
-
 import git
 
-from aider.dump import dump  # noqa: F401
-
-
-class IgnorantTemporaryDirectory:
+class BetterTemporaryDirectory:
     def __init__(self):
         self.temp_dir = tempfile.TemporaryDirectory()
 
@@ -19,8 +15,10 @@ class IgnorantTemporaryDirectory:
         except OSError:
             pass  # Ignore errors (Windows)
 
+    def __str__(self):
+        return self.temp_dir.name
 
-class ChdirTemporaryDirectory(IgnorantTemporaryDirectory):
+class ChdirBetterTemporaryDirectory(BetterTemporaryDirectory):
     def __init__(self):
         self.cwd = os.getcwd()
         super().__init__()
@@ -34,15 +32,18 @@ class ChdirTemporaryDirectory(IgnorantTemporaryDirectory):
         os.chdir(self.cwd)
         super().__exit__(exc_type, exc_val, exc_tb)
 
+    def __str__(self):
+        return self.temp_dir.name
 
-class GitTemporaryDirectory(ChdirTemporaryDirectory):
+class GitBetterTemporaryDirectory(ChdirBetterTemporaryDirectory):
     def __enter__(self):
         res = super().__enter__()
-        make_repo()
+        self.make_repo()
         return res
 
+    def make_repo(self):
+        repo = git.Repo.init()
+        repo.config_writer().set_value("user", "name", "Test User").release()
+        repo.config_writer().set_value("user", "email", "testuser@example.com").release()
 
-def make_repo():
-    repo = git.Repo.init()
-    repo.config_writer().set_value("user", "name", "Test User").release()
-    repo.config_writer().set_value("user", "email", "testuser@example.com").release()
+class
