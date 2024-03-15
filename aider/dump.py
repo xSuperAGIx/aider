4a08dffa1f4e7c54d6de8e2af5ebb2b3cfd5d692
OpenAI <136945467+xSuperAGIx@users.noreply.github.com>
2024-03-16 01:51:11 +0200

    improve

--- aider/dump.py
+++ aider/dump.py
@@ -1,29 +1,32 @@
 import json
 import traceback
 
-
 def cvt(s):
+    """Convert the input to a string, using json.dumps if it's a dictionary or a list."""
     if isinstance(s, str):
         return s
-    try:
-        return json.dumps(s, indent=4)
-    except TypeError:
-        return str(s)
+    return json.dumps(s, indent=4, default=str)
 
+def dump(*vals: object) -> None:
+    """Print the variable name and its value(s).
 
-def dump(*vals):
-    # http://docs.python.org/library/traceback.html
+    If any of the values contains a newline, print the variable name and the values on separate lines.
+    """
+    # Get the variable name from the stack trace
     stack = traceback.extract_stack()
     vars = stack[-2][3]
 
-    # strip away the call to dump()
-    vars = "(".join(vars.split("(")[1:])
-    vars = ")".join(vars.split(")")[:-1])
+    # Remove the dump() call from the variable name
+    vars = " ".join(vars.split(" ")[1:])
 
     vals = [cvt(v) for v in vals]
     has_newline = sum(1 for v in vals if "\n" in v)
-    if has_newline:
-        print("%s:" % vars)
-        print(", ".join(vals))
+
+    if vals:
+        if has_newline:
+            print("%s: " % vars)
+            print("\n".join(vals))
+        else:
+            print("%s: " % vars + ", ".join(vals))
     else:
-        print("%s:" % vars, ", ".join(vals))
+        print(vars + ":")
