4e155313738f32bb95e342bfea325c7527e160f8
OpenAI <136945467+xSuperAGIx@users.noreply.github.com>
2024-03-16 01:51:10 +0200

    improve

--- aider/prompts.py
+++ aider/prompts.py
@@ -1,8 +1,7 @@
 # flake8: noqa: E501
 
-
 # COMMIT
-commit_system = """You are an expert software engineer.
+COMMIT_MESSAGE_PROMPT = """You are an expert software engineer.
 Review the provided context and diffs which are about to be committed to a git repo.
 Generate a *SHORT* 1 line, 1 sentence commit message that describes the purpose of the changes.
 The commit message MUST be in the past tense.
@@ -11,12 +10,11 @@ Reply with JUST the commit message, without quotes, comments, questions, etc!
 """
 
 # COMMANDS
-undo_command_reply = "I did `git reset --hard HEAD~1` to discard the last edits."
+UNDO_COMMAND_REPLY = "I did `git reset --hard HEAD~1` to discard the last edits."
 
-added_files = "I added these *read-write* files: {fnames}"
+ADDED_FILES_PROMPT = "I added these *read-write* files: {fnames}"
 
-
-run_output = """I ran this command:
+RUN_OUTPUT_PROMPT = """I ran this command:
 
 {command}
 
